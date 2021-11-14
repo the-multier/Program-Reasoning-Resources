4865fec1492124aa50f80c73c1b6637dcc0d1a14
the-multier <88647502+the-multier@users.noreply.github.com>
2021-11-14 22:56:54 +0530

    Fixed Error

--- Assignment-6/p3q1.c
+++ Assignment-6/p3q1.c
@@ -1,26 +1,17 @@
-/*@
-    requires n>0 && n<7;
-    ensures 6*\result ==(n*(n+1)*(2*n+1));
+/*@ requires n > 0;
+ensures\result == (n*(n+1)*((2*n)+1))/6;
 */
-
-int sigma(int n)
-{
-    int sum = 0;
-    int i = 1;
-    /*@ 
-        loop invariant 6*sum == (i)(i-1)(2*i-1);
-        loop invariant 1<= i <= n+1;
-        loop assigns sum,i;
-        loop variant n-i;
-    */
-    while(i<=n)
-    {
-        
-        sum = i*i + sum;
-        i = i+1;
-        
-    }
-    
-    return sum;
-
+int sigma(int n) {
+int s = 0;
+int i = 1;
+/*@
+loop invariant s==((i-1)i((2*i)-1))/6;
+loop invariant i<=i<=n+1;
+loop assigns s, i;
+*/
+while (i <= n) {
+s = s +(i*i);
+i = i + 1;
+}
+return s;
 }
